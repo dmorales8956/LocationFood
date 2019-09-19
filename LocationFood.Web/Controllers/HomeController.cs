e7bca76151bb6468fb3d728ce4a84873a5122509
DIANA MARIA MORALES MORALES <dianamorales224800@correo.itm.edu.co>
2019-09-19 10:33:19 -0500

    Create BD

--- LocationFood.Web/Controllers/HomeController.cs
+++ LocationFood.Web/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace LocationFood.Web.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+           return View();
         }
 
         public IActionResult About()
