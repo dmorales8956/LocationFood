d17f0198b624f358746af049b2a9ab21da8487e8
DIANA MARIA MORALES MORALES <dianamorales224800@correo.itm.edu.co>
2019-11-06 11:39:56 -0500

    Change and profile Password

--- LocationFood.Prism/LocationFood.Prism/App.xaml.cs
+++ LocationFood.Prism/LocationFood.Prism/App.xaml.cs
@@ -34,7 +34,7 @@ namespace LocationFood.Prism
             }
             else
             {
-                await NavigationService.NavigateAsync("/NavigationPage/LoginPage");
+                await NavigationService.NavigateAsync("/NavigationPage/MapPage");
             }
         }
 
@@ -49,6 +49,8 @@ namespace LocationFood.Prism
             containerRegistry.RegisterForNavigation<RestaurantsPage, RestaurantsPageViewModel>();
             containerRegistry.RegisterForNavigation<MapPage, MapPageViewModel>();
             containerRegistry.RegisterForNavigation<ReservationPage, ReservationPageViewModel>();
+            containerRegistry.RegisterForNavigation<ChangePasswordPage, ChangePasswordPageViewModel>();
+            containerRegistry.RegisterForNavigation<ProfilePage, ProfilePageViewModel>();
         }
     }
 }
---
+++ LocationFood.Prism/LocationFood.Prism/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,147 @@
+﻿using LocationFood.Common.Helpers;
+using LocationFood.Common.Models;
+using LocationFood.Common.Services;
+using Newtonsoft.Json;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationFood.Prism.ViewModels
+{
+    public class ProfilePageViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IApiService _apiService;
+        private bool _isRunning;
+        private bool _isEnabled;
+        private CustomerResponse _customer;
+        private DelegateCommand _saveCommand;
+        private DelegateCommand _changePasswordCommand;
+
+        public ProfilePageViewModel(
+            INavigationService navigationService,
+            IApiService apiService) : base(navigationService)
+        {
+            _navigationService = navigationService;
+            _apiService = apiService;
+            Title = "Modify Profile";
+            IsEnabled = true;
+            Customer = JsonConvert.DeserializeObject<CustomerResponse>(Settings.Customer);
+        }
+
+        public DelegateCommand ChangePasswordCommand => _changePasswordCommand ?? (_changePasswordCommand = new DelegateCommand(ChangePassword));
+
+        public DelegateCommand SaveCommand => _saveCommand ?? (_saveCommand = new DelegateCommand(Save));
+
+        public CustomerResponse Customer
+        {
+            get => _customer;
+            set => SetProperty(ref _customer, value);
+        }
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => SetProperty(ref _isRunning, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => SetProperty(ref _isEnabled, value);
+        }
+
+        public IApiService ApiService => _apiService;
+
+        private async void Save()
+        {
+            var isValid = await ValidateData();
+            if (!isValid)
+            {
+                return;
+            }
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            var userRequest = new UserRequest
+            {
+                Address = Customer.Address,
+                Document = Customer.Document,
+                Email = Customer.Email,
+                FirstName = Customer.FirstName,
+                LastName = Customer.LastName,
+                Password = "123456", // It doesn't matter what is sent here. It is only for the model to be valid
+                Phone = Customer.PhoneNumber
+            };
+
+            var token = JsonConvert.DeserializeObject<TokenResponse>(Settings.Token);
+
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var response = await ApiService.PutAsync(
+                url,
+                "/api",
+                "/Account",
+                userRequest,
+                "bearer",
+                token.Token);
+
+            IsRunning = false;
+            IsEnabled = true;
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert(
+                    "Error",
+                    response.Message,
+                    "Accept");
+                return;
+            }
+
+            Settings.Customer = JsonConvert.SerializeObject(Customer);
+
+            await App.Current.MainPage.DisplayAlert(
+                "Ok",
+                "User updated succesfully.",
+                "Accept");
+        }
+
+        private async Task<bool> ValidateData()
+        {
+            if (string.IsNullOrEmpty(Customer.Document))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a document.", "Accept");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Customer.FirstName))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a FirstName.", "Accept");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Customer.LastName))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter a LastName.", "Accept");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(Customer.Address))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "You must enter an Address.", "Accept");
+                return false;
+            }
+
+            return true;
+        }
+
+        private async void ChangePassword()
+        {
+            await _navigationService.NavigateAsync("ChangePasswordPage");
+        }
+    }
+}
