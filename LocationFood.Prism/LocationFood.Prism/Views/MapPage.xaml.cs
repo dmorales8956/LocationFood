b85f78638a6902f32db17ba17366722f85ca69b8
DIANA MARIA MORALES MORALES <dianamorales224800@correo.itm.edu.co>
2019-11-09 14:26:27 -0500

    create Map

--- LocationFood.Prism/LocationFood.Prism/Views/MapPage.xaml.cs
+++ LocationFood.Prism/LocationFood.Prism/Views/MapPage.xaml.cs
@@ -6,23 +6,12 @@ namespace LocationFood.Prism.Views
 {
     public partial class MapPage : ContentPage
     {
-
-        private readonly IGeolocatorService _geolocatorService;
-        public MapPage(IGeolocatorService geolocatorService)
+       
+        public MapPage()
         {
             InitializeComponent();
-            _geolocatorService = geolocatorService;
-            MoveMapToCurrentPositionAsync();
+            
         }
-        private async void MoveMapToCurrentPositionAsync()
-        {
-            await _geolocatorService.GetLocationAsync();
-            var position = new Position(
-                _geolocatorService.Latitude,
-                _geolocatorService.Longitude);
-            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(
-            position,
-            Distance.FromKilometers(.5)));
-        }
+       
     }
 }
