b1c59c03f198c7bd9029cb25a89b232f24f45a2f
DIANA MARIA MORALES MORALES <dianamorales224800@correo.itm.edu.co>
2019-11-13 11:14:05 -0500

    recovery

--- LocationFood.Prism/LocationFood.Prism/Views/MapPage.xaml.cs
+++ LocationFood.Prism/LocationFood.Prism/Views/MapPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using LocationFood.Common.Services;
+﻿using LocationFood.Common.Helpers;
+using LocationFood.Common.Models;
+using LocationFood.Common.Services;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
@@ -9,12 +13,49 @@ namespace LocationFood.Prism.Views
     public partial class MapPage : ContentPage
     {
         private readonly IGeolocatorService _geolocatorService;
-        public MapPage(IGeolocatorService geolocatorService)
+        private readonly IApiService _apiService;
+
+        public MapPage(IGeolocatorService geolocatorService, IApiService apiService)
         {
             InitializeComponent();
             _geolocatorService = geolocatorService;
+            _apiService = apiService;
+            ShowRestaurantsAsync();
             MoveMapToCurrentPositionAsync();
-          
+
+        }
+        private async void ShowRestaurantsAsync()
+        {
+            //Aqui consume la url de azure
+            var url = App.Current.Resources["UrlAPI"].ToString();
+            var token = JsonConvert.DeserializeObject<TokenResponse>(Settings.Token);
+
+
+            var response = await _apiService.GetListAsync<RestaurantResponse>(
+                url,
+                "api",
+                "/AdminRestaurants/GetRestaurants",
+                "bearer",
+                token.Token);
+
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var restaurants = (List<RestaurantResponse>)response.Result;
+
+            foreach (var restaurant in restaurants)
+            {
+                MyMap.Pins.Add(new Pin
+                {
+                    Address = restaurant.Address,
+                    Label = restaurant.Name,
+                    Position = new Position(restaurant.Latitude, restaurant.Longitude),
+                    Type = PinType.Place
+                });
+            }
         }
         private async void MoveMapToCurrentPositionAsync()
         {
---
+++ LocationFood.Web/Migrations/20191112035912_CreateNewBD.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocationFood.Web.Migrations
+{
+    public partial class CreateNewBD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantImage_Restaurants_RestaurantId",
+                table: "RestaurantImage");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RestaurantImage",
+                table: "RestaurantImage");
+
+            migrationBuilder.RenameTable(
+                name: "RestaurantImage",
+                newName: "RestaurantImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantImage_RestaurantId",
+                table: "RestaurantImages",
+                newName: "IX_RestaurantImages_RestaurantId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RestaurantImages",
+                table: "RestaurantImages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantImages_Restaurants_RestaurantId",
+                table: "RestaurantImages",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RestaurantImages_Restaurants_RestaurantId",
+                table: "RestaurantImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RestaurantImages",
+                table: "RestaurantImages");
+
+            migrationBuilder.RenameTable(
+                name: "RestaurantImages",
+                newName: "RestaurantImage");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_RestaurantImages_RestaurantId",
+                table: "RestaurantImage",
+                newName: "IX_RestaurantImage_RestaurantId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RestaurantImage",
+                table: "RestaurantImage",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RestaurantImage_Restaurants_RestaurantId",
+                table: "RestaurantImage",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
